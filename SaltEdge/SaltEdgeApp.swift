5250a8ca495f475ef62a4e0f5099d6e987bfeb34
matteoiscrying <67237370+matteoiscrying@users.noreply.github.com>
2024-12-16 19:12:42 +1100

    Initial Commit

---
+++ SaltEdge/SaltEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaltEdgeApp.swift
+//  SaltEdge
+//
+//  Created by Matteo Mekhail on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SaltEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaltEdgeTests/SaltEdgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SaltEdgeTests.swift
+//  SaltEdgeTests
+//
+//  Created by Matteo Mekhail on 16/12/24.
+//
+
+import Testing
+@testable import SaltEdge
+
+struct SaltEdgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
